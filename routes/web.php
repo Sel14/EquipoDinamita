c4d76ba052da6259a8351785e07523df689c7a4b
JohnCaceres <43764825+JohnCaceres@users.noreply.github.com>
2018-12-05 18:27:25 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/project', 'PagesController@admin');
 Route::get('/admin/chat', 'PagesController@chat');
 Route::get('/embarcaciones', 'PagesController@show');
 Route::get('/project/create', 'PagesController@create');
 Route::post('/project', 'PagesController@user');
-/*
-Route::get('/project/{project}/edit', 'PagesController@edit');
+Route::patch('/chat/{project}', 'PagesController@message');
 
-Route::delete('/project/{project}', 'PagesController@delete');
-*/
+Route::patch('/project/{project}/message', 'PagesController@message');
 Route::get('/project/{project}', 'PagesController@fishman');
 Route::get('/project/{project}/chat', 'PagesController@FishmanChat');
 Route::get('/project/{project}/edit', 'PagesController@edit');
 Route::patch('/project/{project}', 'PagesController@update');
+Route::get('/admin/login', 'AdministratorsController@showLoginForm');
+    Route::post('/admin/login', 'AdministratorsController@Login');
+
+
+
+
+    
+    Auth::routes();
+        Route::get('/home', 'HomeController@index')->name('home');
